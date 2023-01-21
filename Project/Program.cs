8df32d6baa96ef2bc36726690e9e04011fb50a12
A S L A N B E K <learneraslanbek1220@gmail.com>
2023-01-21 14:36:52 +0500

    if 19

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+int d = int.Parse(Console.ReadLine());
+if(a == b && a == c && a != d)
+{
+    Console.WriteLine("Teng bulmagan sonning tartib raqami: " + 4); 
+}
+else if(a == b &&b == d && a != c)
+{
+    Console.WriteLine("Teng bulmagan sonning tartib raqami: " + 3);
+}
+else if (a == c && c == d && a != b)
+{
+    Console.WriteLine("Teng bulmagan sonning tartib raqami: " + 2);
+}
+else if(b == c && c == d && c != a)
+{
+    Console.WriteLine("Teng bulmagan sonning tartib raqami: " + 1);
+}
+else
+{
+    Console.WriteLine("teng bulmagan son yuq");
+}
