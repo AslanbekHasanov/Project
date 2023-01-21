c44a73c5df2e2d9f9bb81c2a0dc6e536f5b7eca4
A S L A N B E K <learneraslanbek1220@gmail.com>
2023-01-21 15:25:03 +0500

    if 20

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,21 +1,21 @@
-﻿int a = int.Parse(Console.ReadLine());
-int b = int.Parse(Console.ReadLine());
-int c = int.Parse(Console.ReadLine());
-//int d = int.Parse(Console.ReadLine());
-if (Math.Abs(a - b) > Math.Abs(a - c))
-{
-    Console.WriteLine("YAQIN NUQTA: " + b + "\nmasofasi: " + Math.Abs(a - c));
-}
-else if (Math.Abs(a - b) < Math.Abs(a - c))
-{
-    Console.WriteLine("YAQIN NUQTA: " + b + "\nmasofasi: " + Math.Abs(a - b));
-}
+﻿//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//int c = int.Parse(Console.ReadLine());
+////int d = int.Parse(Console.ReadLine());
+//if (Math.Abs(a - b) > Math.Abs(a - c))
+//{
+//    Console.WriteLine("YAQIN NUQTA: " + b + "\nmasofasi: " + Math.Abs(a - c));
+//}
+//else if (Math.Abs(a - b) < Math.Abs(a - c))
+//{
+//    Console.WriteLine("YAQIN NUQTA: " + b + "\nmasofasi: " + Math.Abs(a - b));
+//}
 
-else if (Math.Abs(a - b) == Math.Abs(a - c))
-{
-    Console.WriteLine("yaqin nuqta: " + b + " " + c);
-}
-else if (a == b && b == c) 
-{
-    Console.WriteLine("nuqtalar ustma ust tushadi");
-}
+//else if (Math.Abs(a - b) == Math.Abs(a - c))
+//{
+//    Console.WriteLine("yaqin nuqta: " + b + " " + c);
+//}
+//else if (a == b && b == c) 
+//{
+//    Console.WriteLine("nuqtalar ustma ust tushadi");
+//}
